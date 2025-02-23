5b874555388024fc7082d1d8a9309241aca4f2de
RosiRosinante <armychawakorn@gmail.com>
2025-02-23 16:12:43 +0700

    Update b.php

--- tasks/b.php
+++ tasks/b.php
@@ -7,12 +7,12 @@ if (!isset($_GET['id'])) {
 }
 
 $id = $_GET['id'];
-$id = intval($id); // Try to sanitize
+// $id = intval($id); // คำตอบ
 
 $sql = "SELECT * FROM users WHERE id = $id"; // Still vulnerable!
 $user = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
 
 echo "<h1>User Details</h1>";
 echo "<p>ID: " . $user['id'] . "</p>";
-echo "<p>Name: " . $user['name'] . "</p>";
+echo "<p>Name: " . $user['username'] . "</p>";
 echo "<p>Email: " . $user['email'] . "</p>";
